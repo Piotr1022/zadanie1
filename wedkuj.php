90d4a18d2e89a03aad5df78a35055d91ad825c59
Piotr Kur <p.szkola132@gmail.com>
2023-04-26 10:37:04 +0200

    wedkuj.php

---
+++ wedkuj.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl_1.css">
+    <title>Wędkowanie</title>
+</head>
+<body>
+    
+    <header>
+        <h1>Portal dla wędkarzy</h1>
+    </header>
+
+
+    <aside>
+        <h3>Ryby zamieszkujące rzeki</h3>
+        <ol>
+
+        </ol>
+    </aside>
+
+    <main>
+        <h3>Ryby drapieżne naszych wód</h3>
+    </main>
+
+    <nav>
+        <img src="ryba1.jpg" alt="Sum">
+        <a href="kwerendy.txt">Pobierz kwerendy</a>
+    </nav>
+
+    <footer>
+        <p>Stronę wykonał: 00000000000</p>
+    </footer>
+
+</body>
+</html>
